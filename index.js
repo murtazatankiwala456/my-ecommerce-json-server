b874ba5fdc7fa305c7d248ac1ccd1895aad50c2f
Murtaza Tankiwala <115138620+murtazatankiwala456@users.noreply.github.com>
2024-07-29 00:20:27 +0530

    setting header X-Total-Count

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
+// Custom middleware to add headers
+server.use((req, res, next) => {
+  if (req.method === "GET" && req.path === "/products") {
+    const data = router.db.get("products").value();
+    res.header("X-Total-Count", data.length); // Set the custom header
+  }
+  next();
+});
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+  console.log("server started ");
+});
